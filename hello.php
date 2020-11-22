d4f89ef439fcbfbca496dd35b04954d39a6cf875
Marta Pancaldi <marta.panc@gmail.com>
2020-11-22 00:02:46 +0000

    Add basic examples

--- hello.php
+++ hello.php
@@ -1,4 +1,18 @@
 <?php
-    echo 'Hello world!';
+    $hello_world = 'Hello world!';
+    $x = 'Pretty'; $x .= 'Cool';
+    echo $hello_world;
+    echo "<br/>";
+    echo PHP_INT_MAX;
+    echo "<br/>";
+    echo "PHP" . " ROCKS" . 11 . 5;
+    echo '1 == 1'. 1 == '1';
+    $equal = '1 == "1":' . 1 == '1';
+    echo $equal;
+    echo "<br/>";
+    $identical = "1 === \"1\":" . 1 === "1";
+    echo $identical;
+    echo "<br/>";
+
 
 
